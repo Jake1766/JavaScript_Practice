853e88bb29c537032d06e967cefc870e359ee766
Jake1766 <jakerandall1766@gmail.com>
2025-02-11 09:07:17 +0000

    created class selectors for text, added background color for div sections

--- small_things/app.js
+++ small_things/app.js
@@ -1,3 +1,17 @@
-function deep(){
-    console.log("bonk")
+// here are some basic problems found at 
+// https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Strings
+// I'll keep them all contained within seperate functions
+
+
+function declaring_string(){
+    const string = "this is a string!";
+    console.log(string);
+}
+
+// Template literals are similar to fstrings in python
+// they allow for embedded JS
+
+function template_literal() {
+    const word = "beep";
+    console.log(`here is a word: ${word}`);
 }
