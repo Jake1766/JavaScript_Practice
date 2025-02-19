e72615dae72c3a423bd45f42b1bb2457d393509f
Jake1766 <jakerandall1766@gmail.com>
2025-02-19 09:30:44 +0000

    added js to create and add red p element

--- small_things/DOMManipulation/app.js
+++ small_things/DOMManipulation/app.js
@@ -1,6 +1,19 @@
 const container = document.querySelector("#container");
 
-const content = document.createElement("div");
+var content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the text content of div #container";
+container.appendChild(content);
+
+
+content = document.createElement("p");
+content.classList.add("content");
+content.textContent = "Hey, I'm red!";
+content.style.color = 'red';
+container.appendChild(content);
+
+content = document.createElement('h3');
+content.classList.add('content');
+content.textContent('I\'m a blue h3');
+content.style.color = 'blue';
 container.appendChild(content);
