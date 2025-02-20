63234238dfeacd7178d4795493eb4c9d9296b880
Jake1766 <jakerandall1766@gmail.com>
2025-02-20 10:02:26 +0000

    working on adding delete button to remove list items:

--- shoppingList/app.js
+++ shoppingList/app.js
@@ -1,9 +1,15 @@
 const inputText = document.querySelector("#inputText");
-const button = document.querySelector("#btn")
+const button = document.querySelector("#btn");
 
-const list = document.querySelector("#list")
+const list = document.querySelector("#list");
 
-button.addEventListener('click', addItem)
+button.addEventListener('click', addItem);
+
+function deleteItem(item){
+    console.log('deleting: ', item);
+    var item = document.querySelector(`#${item}`);
+    item.remove();
+}
 
 
 function addItem(){
@@ -11,8 +17,26 @@ function addItem(){
     console.log("adding item", text);
     inputText.value = '';
 
+    
+
+    //create delete button
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+    //add event listener to delete button 
+    deleteButton.addEventListener('click', deleteItem(text));
+
+
+
+    //create list item 
     var listItem = document.createElement('li');
     listItem.textContent = text;
+    listItem.id = listItem.textContent;
+
+
+    //add list item to list
     list.appendChild(listItem);
+
+    //add delete button to list item
+    listItem.appendChild(deleteButton);
     
 } 
