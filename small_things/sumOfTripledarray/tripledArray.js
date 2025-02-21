dc9224da120e7233fe52dad4b6eef0577d86cf8e
Jake1766 <jakerandall1766@gmail.com>
2025-02-21 09:14:24 +0000

    working on fancyTriple()

--- small_things/sumOfTripledarray/tripledArray.js
+++ small_things/sumOfTripledarray/tripledArray.js
@@ -4,3 +4,19 @@
 const nums = [1, 2, 3, 4, 5, 6]
 
 console.log('working');
+
+//function using a for loop
+
+function forLoopTriple(arr){
+    let total = 0;
+    for(var i = 0; i < arr.length; i++){
+        //check if even
+        if(arr[i]%2 == 0){
+            total += arr[i]*3;
+        }
+    }
+    console.log("for loop result: ", total);
+    return total;
+}
+
+forLoopTriple(nums);
