89e94b2adb62f1933332a8734fcd91be5c65620c
Jake1766 <jakerandall1766@gmail.com>
2025-02-21 09:44:03 +0000

    working

--- small_things/sumOfTripledarray/tripledArray.js
+++ small_things/sumOfTripledarray/tripledArray.js
@@ -18,5 +18,22 @@ function forLoopTriple(arr){
     console.log("for loop result: ", total);
     return total;
 }
-
 forLoopTriple(nums);
+
+//same function using .map
+
+function fancyTriple(arr){
+   var evens = arr.filter(num => num%2 == 0);
+   console.log('evens:', evens);
+
+   
+   var triple = evens.reduce((total, currentItem) => {
+   console.log('current: ', currentItem);
+   console.log('total: ', total);
+   console.log('this loop value: ', total + (currentItem * 3));
+   return total + (currentItem * 3);
+   });
+   console.log('triple evens: ', triple);
+}
+
+fancyTriple(nums);
