4a9205011472115ac4949679203aad6a3f397461
Jake1766 <jakerandall1766@gmail.com>
2025-02-22 10:49:08 +0000

    finsihed filter demo

--- small_things/mapFilterReduce/app.js
+++ small_things/mapFilterReduce/app.js
@@ -1,12 +1,19 @@
 const nums = [1, 2, 3, 4, 5,];
 
+// map demo
+
 function addOne(num){
     return num + 1;
 }
 
 const mappedArr = nums.map(addOne);
-alert(mappedArr);
+//alert(mappedArr);
+
+const filteredArr = nums.filter((num) => num%2==0);
+//alert(filteredArr);
+
+//alerts: 2, 4
 
 
-//map demo
+
 
