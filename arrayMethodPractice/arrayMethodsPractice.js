8a9aba8aba6ccc10befb7d8569325c99db0e045a
Jake1766 <jakerandall1766@gmail.com>
2025-03-03 11:19:55 +0000

    added filter method to remove hyphen

--- arrayMethodPractice/arrayMethodsPractice.js
+++ arrayMethodPractice/arrayMethodsPractice.js
@@ -3,17 +3,26 @@
 str = "background-color"
 
 function camelize(str){
-    //split string
-    arr = str.split('');
-    //capitalize first letter second word
-        //identify hyphen
-        //remove hyphen
-    const capitalIndex = arr.indexOf('-');
-    arr.splice(capitalIndex, 1)
-    arr[capitalIndex] = arr[capitalIndex].toUpperCase();
+    //check length > 0
+    console.log(str.length)
+    if (str.length > 0){
+        console.log('working...')
+        //split string
+        arr = str.split('');
 
-    str = arr.join('');
-    return str
+        //use filer method to remove -
+        let filteredArr = arr.filter(item => item != '-')
+        console.log('filtered arr: ', filteredArr);
+
+
+        const capitalIndex = arr.indexOf('-');
+        arr.splice(capitalIndex, 1)
+        arr[capitalIndex] = arr[capitalIndex].toUpperCase();
+
+        str = arr.join('');
+        console.log(str)
+        return str
+    }
 }
 
 camelize(str)
