57c29f064ec673d8776f23e5d0ec9a5b58556371
Jake1766 <jakerandall1766@gmail.com>
2025-03-03 10:40:12 +0000

    outlined camelize function with comments

---
+++ arrayMethodPractice/arrayMethodsPractice.js
@@ -0,0 +1,10 @@
+//write a function to camelize the following string
+
+str = "background-color"
+
+function camelize(){
+    //split string
+    //capitalize first letter second word
+    //remove hyphen
+    //join back together
+}
