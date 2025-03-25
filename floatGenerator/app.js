67edcdf41beb40a0879938b24afceda9f435daba
Jake1766 <jakerandall1766@gmail.com>
2025-03-25 16:45:48 +0000

    float gen finished

--- floatGenerator/app.js
+++ floatGenerator/app.js
@@ -23,26 +23,3 @@ function insertFloat(){
     fieldB.value = floatGen(3.5, 4.2).toFixed(1);
     fieldC.value = floatGen(6.8, 7.5).toFixed(1);
 }
-
-function copy(element){
-    const textToCopy = Element.value;
-    navigator.clipboard.writeText(textToCopy);
-}
-
-copyA.addEventListener("click", () => {
-    copy(fieldA)
-    .then(() => {
-        alert("text copied to clipboard")
-    })
-    .catch(err => {
-        console.error("error copying text: ", err)
-    })
-});
-
-copyB.addEventListener("click", () => {
-    copy(fieldB);
-});
-
-copyC.addEventListener("click", () => {
-    copy(fieldC);
-});
